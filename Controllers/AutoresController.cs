584cc177e59c54fac46b533c4a5d5afbb2dcd145
José Manuel Sánchez Juárez <53586710+josemanuelwar@users.noreply.github.com>
2026-05-21 13:44:51 -0600

    Explicasion

--- Controllers/AutoresController.cs
+++ Controllers/AutoresController.cs
@@ -30,7 +30,18 @@ namespace BibliotecaAPI.Controllers
         }
 
         [HttpGet("{id:int}")]
-        public async Task<ActionResult<Autor>> Get(int id)
+
+        /**
+        * obtener datos de la cabecera de la petición
+         * [FromHeader] string valorCabecera
+         * 
+         * obtener datos de la cadena de consulta
+         * [FromQuery] string valorQuery
+         * 
+         * obtener datos del url de la petición
+         * [FromRoute] string valorCuerpo
+        */
+        public async Task<ActionResult<Autor>> Get([FromRoute] int id, [FromQuery] bool incluirLibros)
         {
             var autor =  await this.context.Autores.Include(x => x.libros).FirstOrDefaultAsync(x => x.Id == id);
             if(autor is null)
@@ -40,14 +51,20 @@ namespace BibliotecaAPI.Controllers
             return autor;
         }
 
-        [HttpGet("{parametro1}/{paramentro2?}")] // parametro opcional
+        [HttpGet("{nombre:alpha}")]
+        public async Task<IEnumerable<Autor>>Get([FromRoute] string nombre)
+        {
+            return await this.context.Autores.Where(x => x.Nombre.Contains(nombre)).ToListAsync();
+        }
+
+        /*[HttpGet("{parametro1}/{paramentro2?}")] // parametro opcional
         public ActionResult Get(string parametro1, string? paramentro2)
         {
             return Ok(new {parametro1,paramentro2});
-        }
+        }*/
 
         [HttpPost]
-        public async Task<ActionResult> Post(Autor autor)
+        public async Task<ActionResult> Post([FromBody]Autor autor)
         {
             this.context.Add(autor);
             await this.context.SaveChangesAsync();
@@ -56,7 +73,7 @@ namespace BibliotecaAPI.Controllers
         }
 
         [HttpPut("{id:int}")]
-        public async Task<ActionResult> Put(int id, Autor autor)
+        public async Task<ActionResult> Put([FromRoute] int id, [FromBody]Autor autor)
         {
             if(id != autor.Id)
             {
@@ -68,7 +85,7 @@ namespace BibliotecaAPI.Controllers
         }
 
         [HttpDelete("{id:int}")]
-        public async Task<ActionResult> Delete(int id)
+        public async Task<ActionResult> Delete([FromRoute] int id)
         {
             var registrosBorrados = await this.context.Autores.Where(x => x.Id == id).ExecuteDeleteAsync();
             if( registrosBorrados == 0)
