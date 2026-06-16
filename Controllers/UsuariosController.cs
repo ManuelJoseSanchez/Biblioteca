d98a37cf1c890b0beaea03dfa571f008fe7ec460
José Manuel Sánchez Juárez <53586710+josemanuelwar@users.noreply.github.com>
2026-06-16 13:21:33 -0600

    agregando el login y registro de usuario

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -1,6 +1,11 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Biblioteca.DTOs;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Biblioteca.Controllers
 {
@@ -11,11 +16,99 @@ namespace Biblioteca.Controllers
     {
         private readonly UserManager<IdentityUser> userManager;
         private readonly IConfiguration configuration;
+        private readonly SignInManager<IdentityUser> signInManager;
 
-        public UsuariosController(UserManager<IdentityUser> userManager, IConfiguration configuration)
+        public UsuariosController(UserManager<IdentityUser> userManager, IConfiguration configuration, SignInManager<IdentityUser> signInManager)
         {
             this.userManager = userManager;
             this.configuration = configuration;
+            this.signInManager = signInManager;
+        }
+
+        [HttpPost("registro")]
+        [AllowAnonymous]
+        public async Task<ActionResult<RespuestaAutenticacionDTO>> Registro(CredencialesUsuarioDTO credencialesUsuarioDTO)
+        {
+            var usuario = new IdentityUser
+            {
+                UserName = credencialesUsuarioDTO.Email,
+                Email = credencialesUsuarioDTO.Email,
+            };
+
+            var resultado = await this.userManager.CreateAsync(usuario, credencialesUsuarioDTO.Password!);
+
+            if (resultado.Succeeded)
+            {
+                var respuestaAutenticacion = await this.ConstruirToken(credencialesUsuarioDTO);
+                return respuestaAutenticacion;
+            }
+            else
+            {
+                foreach (var error in resultado.Errors)
+                {
+                    ModelState.AddModelError(string.Empty,error.Description);
+                }
+                return ValidationProblem();
+            }
+        }
+
+        [HttpPost("login")]
+        [AllowAnonymous]
+        public async Task<ActionResult<RespuestaAutenticacionDTO>> Login(CredencialesUsuarioDTO credencialesUsuarioDTO)
+        {
+            var usuario = await this.userManager.FindByEmailAsync(credencialesUsuarioDTO.Email);
+
+            if(usuario is null)
+            {
+                return this.RetornarLoginIncorrecto();
+            }
+
+            var resultado = await this.signInManager.CheckPasswordSignInAsync(usuario,credencialesUsuarioDTO.Password!, lockoutOnFailure: false);
+
+            if (resultado.Succeeded)
+            {
+                return await this.ConstruirToken(credencialesUsuarioDTO);
+            }
+            else
+            {
+                return this.RetornarLoginIncorrecto();
+            }
+
+        }
+
+        private ActionResult RetornarLoginIncorrecto()
+        {
+            ModelState.AddModelError(string.Empty, "Login incorrecto");
+            return ValidationProblem();
+        }
+
+        private async Task<RespuestaAutenticacionDTO> ConstruirToken( CredencialesUsuarioDTO credencialesUsuarioDTO)
+        {
+            var claims =  new List<Claim>
+            {
+                new Claim("email", credencialesUsuarioDTO.Email),
+                new Claim("lo que yo quiera","cualquier valor")
+            };
+
+            var usuario = await this.userManager.FindByEmailAsync(credencialesUsuarioDTO.Email);
+            var claimsDB = await this.userManager.GetClaimsAsync(usuario!);
+
+            claims.AddRange(claimsDB);
+
+            var llave = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(this.configuration["llavejwt"]!));
+            var credenciales =  new SigningCredentials(llave,SecurityAlgorithms.HmacSha256);
+
+            var expiracion =  DateTime.UtcNow.AddDays(1);
+
+            var tokenDeSeguridad= new JwtSecurityToken(issuer: null, audience: null, claims:claims, expires: expiracion, signingCredentials:credenciales);
+
+            var token = new JwtSecurityTokenHandler().WriteToken(tokenDeSeguridad);
+
+            return new RespuestaAutenticacionDTO
+            {
+                Token= token,
+                Expiracion=expiracion
+            };
         }
 
 
